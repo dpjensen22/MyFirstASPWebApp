1ecb5eab937db624c0adae9f732c3b4a0e6adedb
dpjensen22 <dpjensen22@gmail.com>
2022-01-19 20:46:54 -0700

    fixed calculator

--- MyFirstASPWebApp/wwwroot/js/calculator.js
+++ MyFirstASPWebApp/wwwroot/js/calculator.js
@@ -1,41 +1,36 @@
-﻿/*/*This calculator wont work for some reason and it cant figure it out.*/*/
+﻿$("#submit").click(function () {
+    finalPercent =
+        ($("#assignments").val() * .55 +
+        $("#groupprojects").val() * .05 +
+        $("#quizzes").val() * .10 +
+        $("#exams").val() * .20 +
+        $("#intex").val() * .10) * 100
 
-function calculate() {
+    if (finalPercent >= 94) {
+        letterGrade = "A"
+    } else if (finalPercent >= 90) {
+        letterGrade = "A-"
+    } else if (finalPercent >= 87) {
+        letterGrade = "B+"
+    } else if (finalPercent >= 84) {
+        letterGrade = "B"
+    } else if (finalPercent >= 80) {
+        letterGrade = "B-"
+    } else if (finalPercent >= 77) {
+        letterGrade = "C+"
+    } else if (finalPercent >= 74) {
+        letterGrade = "C"
+    } else if (finalPercent >= 70) {
+        letterGrade = "C-"
+    } else if (finalPercent >= 67) {
+        letterGrade = "D+"
+    } else if (finalPercent >= 64) {
+        letterGrade = "D"
+    } else if (finalPercent >= 60) {
+        letterGrade = "D-"
+    } else {
+        letterGrade = "E"
+    }
 
-    var A = $("#assignments").val;
-    var GP = $("#groupprojects").val;
-    var Q = $("#quizzes").val;
-    var E = $("#exams").val;
-    var I = $("#intex").val;
-    var FG = 0;
-    var FLG = "";
-
-    FG = (((parseFloat(A) * .55) + (parseFloat(GP) * .05) + (parseFloat(Q) * .1) + (parseFloat(E) * .2) + (parseFloat(I) * .1)) * 100)
-
-    if (FG >= 94)
-        FLG = "A+";
-    else if (FG >= 90)
-        FLG = "A";
-    else if (FG >= 87)
-        FLG = "B+";
-    else if (FG >= 84)
-        FLG = "B";
-    else if (FG >= 80)
-        FLG = "B-";
-    else if (FG >= 77)
-        FLG = "C+";
-    else if (FG >= 74)
-        FLG = "C";
-    else if (FG >= 70)
-        FLG = "C-";
-    else if (FG >= 67)
-        FLG = "D+";
-    else if (FG >= 64)
-        FLG = "D";
-    else if (FG >= 60)
-        FLG = "D-";
-    else FLG = "F";
-
-    $('showdatap').innerHTML = FG;
-    $('showdatalg').innerHTML = FLG;
-}
+    alert(finalPercent + "% " + letterGrade)
+})
